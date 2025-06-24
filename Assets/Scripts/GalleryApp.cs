965ae49b7fa1189b302f85db5a0e827ed4cf94a0
AundeeDCat <119554562+AundeeDCat@users.noreply.github.com>
2025-06-25 01:35:46 +0800

    Made the Drag to Scroll Functionality in Gallery App

--- Assets/Scripts/GalleryApp.cs
+++ Assets/Scripts/GalleryApp.cs
@@ -6,14 +6,19 @@ using DG.Tweening;
 
 public class GalleryApp : MonoBehaviour
 {
+<<<<<<< Updated upstream
     Vector2 scrollDir;
     float scrollSpeed = 0.25f;
+=======
+    [SerializeField] private float scrollWheelSpeed = 50f;
+    [SerializeField] private float scrollDragSpeed = 0.0025f;
+>>>>>>> Stashed changes
 
     float scrollUpperLimit = 0;
     float scrollLowerLimit = 6.25f;
     [SerializeField] GameObject scrollObject;
+    [SerializeField] Rigidbody2D scrollRigidBody;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
@@ -22,11 +27,49 @@ public class GalleryApp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        dragScroll();
+    }
+
+    void mouseScroll()
+    {
+        
+        Vector2 scrollDir;
+
         scrollDir = Mouse.current.scroll.ReadValue();
         if (scrollDir.y > 0 && scrollObject.transform.position.y <= scrollLowerLimit)
-            scrollObject.transform.position += new Vector3(0,scrollSpeed,0);
+            scrollObject.transform.position += new Vector3(0, scrollWheelSpeed, 0);
+
+        if (scrollDir.y < 0 && scrollObject.transform.position.y >= scrollUpperLimit)
+            scrollObject.transform.position -= new Vector3(0, scrollWheelSpeed, 0);
+    }
+
+
+    
+    float mousePosDown;
+
+    void dragScroll()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePosDown = Input.mousePosition.y; // On Press, get the coordinate of the Mouse Position
+        }
+
+        else if (Input.GetMouseButtonUp(0))
+        {
             
-        if (scrollDir.y < 0  && scrollObject.transform.position.y >= scrollUpperLimit)
-            scrollObject.transform.position -= new Vector3(0, scrollSpeed, 0);
+            scrollRigidBody.linearVelocityY = 0;
+        }
+
+        else if (Input.GetMouseButton(0))
+        {
+            if (mousePosDown != Input.mousePosition.y) // While not released, compare the record coordinates to the current coordinates for changes, if different from record, scroll
+            {
+                float scrollDir = (Input.mousePosition.y - mousePosDown);
+
+                scrollRigidBody.linearVelocityY = scrollDir * scrollDragSpeed;
+
+                Debug.Log(scrollDir);
+            }
+        }
     }
 }
