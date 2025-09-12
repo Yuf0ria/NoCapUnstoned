832e3183a30dbaa0bb23a66349458d2ba12599a3
Nicaia Divina <smookiekinskuma@gmail.com>
2025-09-12 23:44:10 +0800

    Minor changes to UI
    Started stretching the UI to make it useable in different screen ratios (somewhat)
    
    - Good part of the Webb is adjusted

--- Assets/(!) Villanueva/App_Basic.cs
+++ Assets/(!) Villanueva/App_Basic.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class App_Basic : MonoBehaviour
 {
     [SerializeField] GameObject AppIcon;
+    [SerializeField] GameObject Position;
 
     //Make a General App that contains general functions like Open and Close
     //Then make a specific App you want to Open and Close that you drop into the slot
@@ -15,14 +16,15 @@ public class App_Basic : MonoBehaviour
 
         transform.position = App_ClosedPoint;
         transform.localScale = ClosedScale;
+        OpenedPoint = Position.transform.position;
     }
 
 
     //Vector3 App_ClosedPoint;
-    Vector3 ClosedScale = new Vector3(0, 0, 0);
-    Vector3 OpenedPoint = new Vector3(540, 960, 0);
-    Vector3 OpenScale = new Vector3(1, 1, 1);
-    float TransitionTime = 0.5f;
+    [SerializeField] Vector3 ClosedScale = new Vector3(0, 0, 0);
+    [SerializeField] Vector3 OpenedPoint; //= new Vector3(540, 960, 0);
+    [SerializeField] Vector3 OpenScale = new Vector3(1, 1, 1);
+    [SerializeField] float TransitionTime = 0.5f;
 
     public static GameObject CurrentApp;
     public static Vector3 App_ClosedPoint;
