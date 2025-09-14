225badc95ba6e18f1c0a405f2cdf162c61e8558a
Nicaia Divina <smookiekinskuma@gmail.com>
2025-09-15 02:07:45 +0800

    Adjusted 90% of Apps & Fixed Window Position
    Adjusted
    - Webb
    - Postmail
    - FriendLink (I'm surprised i did this first)
    - Eduva
    - Order Corner
    - Notes
    
    Apps I have NOT adjusted yet
    - Settings
    - Gallery
    - Messages
    
    More sidenotes:
    I split the bottom panel of the Friendlink from the original one because it's not doing what I want it to do so :P

--- Assets/(!) Villanueva/App_Basic.cs
+++ Assets/(!) Villanueva/App_Basic.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class App_Basic : MonoBehaviour
 {
     [SerializeField] GameObject AppIcon;
-    [SerializeField] GameObject Position;
+    [SerializeField] GameObject Position; //Place STANDARD POSITION GameObject here!
 
     //Make a General App that contains general functions like Open and Close
     //Then make a specific App you want to Open and Close that you drop into the slot
@@ -17,17 +17,28 @@ public class App_Basic : MonoBehaviour
         transform.position = App_ClosedPoint;
         transform.localScale = ClosedScale;
         OpenedPoint = Position.transform.position;
+        // ^^^^
+        //For the position of the app! (I MADE THIS SCRIPT - NICAIA)
+        //MORE SIDE NOTES:
+        //This only saves the position during the start which means if the player resizes-
+        //the game window, it will mess up the position.
     }
 
 
     //Vector3 App_ClosedPoint;
-    [SerializeField] Vector3 ClosedScale = new Vector3(0, 0, 0);
-    [SerializeField] Vector3 OpenedPoint; //= new Vector3(540, 960, 0);
-    [SerializeField] Vector3 OpenScale = new Vector3(1, 1, 1);
-    [SerializeField] float TransitionTime = 0.5f;
+    Vector3 ClosedScale = new Vector3(0, 0, 0);
+    Vector3 OpenedPoint; //= new Vector3(540, 960, 0);
+    Vector3 OpenScale = new Vector3(1, 1, 1);
+    float TransitionTime = 0.5f;
 
     public static GameObject CurrentApp;
     public static Vector3 App_ClosedPoint;
+
+    private void Update() //To update the position everytime the window is resized. I might find a better way idk but this will do :P - Nicaia
+    {
+        this.gameObject.transform.position = Position.transform.position;
+        OpenedPoint = Position.transform.position;
+    }
 
     public void OpenApp()
     {
@@ -39,6 +50,7 @@ public class App_Basic : MonoBehaviour
 
         CurrentApp = this.gameObject;
         App_ClosedPoint = AppIcon.transform.position;
+
     }
 
 }
