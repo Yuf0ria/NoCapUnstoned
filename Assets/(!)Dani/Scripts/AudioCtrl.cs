17db935f3049c657d020ab4dc49c86e77098792c
Daniella Fegi <114934334+Yuf0ria@users.noreply.github.com>
2025-11-27 22:55:46 +0800

    Add audio managers and AudioCtrl to scenes
    Introduced Music Manager and SFX Manager GameObjects to Chapter 1, Main Menu, and Tutorial scenes, each with appropriate AudioSource and control scripts. Added AudioCtrl GameObject to all scenes for centralized audio control. Updated .vscode/settings.json for correct solution name. Minor UI and slider value adjustments were also made.

--- Assets/(!)Dani/Scripts/AudioCtrl.cs
+++ Assets/(!)Dani/Scripts/AudioCtrl.cs
@@ -4,21 +4,21 @@ using UnityEngine.SceneManagement;
 public class AudioCtrl : MonoBehaviour
 {
     //Scripts to call
-    private static AudioCtrl instance;
+    // private static AudioCtrl instance;
     //GameObject
     
-    private void Awake()
-    {
-        if(instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+    // private void Awake()
+    // {
+    //     if(instance == null)
+    //     {
+    //         instance = this;
+    //         DontDestroyOnLoad(gameObject);
+    //     }
+    //     else
+    //     {
+    //         Destroy(gameObject);
+    //     }
+    // }
 
     void Start()
     {
@@ -36,19 +36,17 @@ public class AudioCtrl : MonoBehaviour
     {
         //& a bit of sfx. why? becuase it loops if you put on update and it doesn't have a key
         //Play when open
-        //Change this menu later
-        var Menu  = SceneManager.GetSceneByName("Copy_MainMenu").isLoaded;
-        if(Menu)
-        {
-            MusicManager.Play("Menu");
-            SoundEffectManager.Play("Start");
-        }
         //stop if scene change
         var Tutorial = SceneManager.GetSceneByName("Tutorial").isLoaded;
         if(Tutorial)
         {
-            MusicManager.Stop("Menu");
             MusicManager.Play("tutorial");
+        }
+        var Chap1 = SceneManager.GetSceneByName("Chapter 1").isLoaded;
+        if(Chap1)
+        {
+            MusicManager.Stop("tutorial");
+            MusicManager.Play("Chap1");
         }
 
     }
